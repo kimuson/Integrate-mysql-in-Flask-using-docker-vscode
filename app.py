639f291b8cc14a2843274e9254c3eb1bef05c4f4
kimuson <kimurayut6677@gmail.com>
2021-04-26 02:25:22 +0000

    SQLite database in flask API- P5- Python | Flask tutorials

--- app.py
+++ app.py
@@ -1,11 +1,91 @@
-#-----------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------
 
-from flask import Flask
+import sqlite3
+from sqlite3.dbapi2 import Cursor
+from flask import Flask, json, request, jsonify
+from flask.wrappers import Request
+import sqlite3
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+def db_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect('books.sqlite')
+    except sqlite3.Error as e:
+        print(e)
+    return conn
+
+
+@app.route('/books', methods=['GET', 'POST'])
+def books():
+    conn = db_connection()
+    cursor = conn.cursor()
+    if request.method == 'GET':
+        cursor = conn.execute("SELECT * FROM book")
+        books = [
+            dict(id=row[0], author=row[1], language=row[2],title=row[3])
+            for row in cursor.fetchall()
+        ]
+        if books is not None:
+            return jsonify(books)
+
+    if request.method == 'POST':
+        new_author = request.form['author']
+        new_lang = request.form['language']
+        new_title = request.form['title']
+        sql = """INSERT INTO book (author, language, title)VALUES(?,?,?)"""
+        cursor = cursor.execute(sql, (new_author,new_lang,new_title))
+        conn.commit()
+
+        return f"Book with the id:{cursor.lastrowid}created successfully"
+
+
+@app.route('/books/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def single_book(id):
+    conn = db_connection()
+    cursor = conn.cursor()
+    book = None
+
+    if request.method == 'GET':
+        cursor.execute("SELECT * FROM book WHERE id = ?",(id,))
+        rows = cursor.fetchall()
+        for r in rows:
+            book = r
+        if book is not None:
+            return jsonify(book), 200
+        else:
+            return "Something wrong", 404
+
+    if request.method == 'PUT':
+        sql = """UPDATE book
+                SET title=?,
+                    author=?,
+                    language=?
+                WHERE id=? """
+
+        author = request.form["author"]
+        language = request.form["language"]
+        title = request.form["title"]
+        updated_book = {
+            "id": id,
+            "author": author,
+            "language": language,
+            "title": title,
+        }
+        conn.execute(sql, (author,language, title,id))
+        conn.commit()
+        return jsonify(updated_book)
+
+    if request.method == 'DELETE':
+        sql = """ DELETE FROM book WHERE id=? """
+        conn.execute(sql, (id,))
+        conn.commit()
+        return "The book with id: {} has been ddeleted.".format(id), 200
+        
+
+if __name__ == '__main__':
+    app.run()
